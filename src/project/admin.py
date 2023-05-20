7f996db752ae0e0c6640c54195d31e2972d6350e
yuli <li104.li@gmail.com>
2023-05-20 17:07:21 -0400

    deleted project folder

--- src/project/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ProjectPost
-#
-# # Register your models here.
-#
-admin.site.register(ProjectPost)
--- src/project/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# Models are linked to our database.
-class ProjectPost(models.Model):
-    title = models.TextField()
-    description = models.TextField(null=True, blank=True)
