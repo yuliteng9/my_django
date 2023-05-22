948c1e86aab98936d5033f6fdeb188eabacba920
yuli <li104.li@gmail.com>
2023-05-22 16:11:54 -0400

    error document editted

--- src/cp/views.py
+++ src/cp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import ProjectPost
 
 # Create your views here.
 def project_post_page(request, post_id=1):
-    obj = ProjectPost.objects.get(id=post_id)
+    try:
+        obj = ProjectPost.objects.get(id=post_id)
+    except:
+        raise Http404
     template_name = "project_post_page.html"
     context = {"h1": "Project Post", "project": obj}
     return render(request, template_name, context)
